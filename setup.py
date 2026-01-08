b94a37fe866efc8eaae57a6b2e7162cc76ac232d
QuantumSolver <marcquesmouton1992@gmail.com>
2026-01-08 10:33:54 +0000

    Fix setup.py circular import issue

--- setup.py
+++ setup.py
@@ -1,14 +1,35 @@
+import os
+import re
+
 from setuptools import find_packages, setup
+
+
+def get_version():
+    """Read version from __init__.py without importing the module"""
+    init_path = os.path.join(
+        os.path.dirname(__file__), "frappe_puppeteer_pdf", "__init__.py"
+    )
+
+    if not os.path.exists(init_path):
+        init_path = os.path.join(os.path.dirname(__file__), "__init__.py")
+
+    with open(init_path, "r") as f:
+        content = f.read()
+
+    version_match = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', content, re.M)
+
+    if version_match:
+        return version_match.group(1)
+    else:
+        return "1.0.0"
+
 
 with open("requirements.txt") as f:
     install_requires = f.read().strip().split("\n")
 
-# get version from __version__ variable in frappe_puppeteer_pdf/__init__.py
-from frappe_puppeteer_pdf import __version__ as version
-
 setup(
     name="frappe_puppeteer_pdf",
-    version=version,
+    version=get_version(),
     description="Frappe App to generate PDFs using Puppeteer/Chrome",
     author="Frappe Technologies Pvt Ltd.",
     author_email="hello@frappe.io",
