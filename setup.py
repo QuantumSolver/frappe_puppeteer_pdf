3f0a9971b00d92866d30655517abfcd3771cfb8d
QuantumSolver <marcquesmouton1992@gmail.com>
2026-01-08 10:57:50 +0000

    Use flat structure like standard Frappe apps

--- setup.py
+++ setup.py
@@ -1,28 +1,22 @@
 import os
-import re
 
 from setuptools import find_packages, setup
 
 
+# Get version from __init__.py
 def get_version():
-    """Read version from __init__.py without importing the module"""
-    init_path = os.path.join(
-        os.path.dirname(__file__), "frappe_puppeteer_pdf", "__init__.py"
-    )
-
-    with open(init_path, "r") as f:
-        content = f.read()
-
-    version_match = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', content, re.M)
-
-    if version_match:
-        return version_match.group(1)
-    else:
-        return "1.0.0"
+    with open(os.path.join("frappe_puppeteer_pdf", "__init__.py"), "r") as f:
+        for line in f:
+            if line.startswith("__version__"):
+                return eval(line.split("=")[1].strip())
+    return "1.0.0"
 
 
-with open("requirements.txt") as f:
-    install_requires = f.read().strip().split("\n")
+# Read requirements from requirements.txt
+def get_requirements():
+    with open("requirements.txt", "r") as f:
+        return f.read().strip().split("\n")
+
 
 setup(
     name="frappe_puppeteer_pdf",
@@ -33,5 +27,5 @@ setup(
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
-    install_requires=install_requires,
+    install_requires=get_requirements(),
 )
